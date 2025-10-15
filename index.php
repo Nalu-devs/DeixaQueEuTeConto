e859b7db0b3a69e7ac7cbca1d53d9f756dd2a665
Aluno.Etec <aula@etec.com>
2025-10-14 22:18:52 -0300

    .

--- index.php
+++ index.php
@@ -1,30 +1,45 @@
 <?php
+$imgDir = 'img/';
+$imgFiles = array_values(array_diff(scandir($imgDir), array('.', '..'))); // corrigido aqui
 $audioDir = 'audios/';
-$audioFiles = array_diff(scandir($audioDir), array('.', '..'));
-//$audioFiles = scandir($audioDir);
+$audioFiles = array_values(array_diff(scandir($audioDir), array('.', '..')));
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reprodutor de Áudio</title>
+    <title>Deixa que eu te conto</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Selecione um áudio para ouvir</h1>
-    <ul>
-        <?php foreach ($audioFiles as $audioFile): ?>
-            <li>
-                <div onclick="playAudio('<?php echo $audioFile; ?>')"><?php echo $audioFile; ?></div>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    <h1>Deixa que eu te conto</h1>
+    <p>Selecione um número e ouça seu conto</p>
 
-    <audio id="audioPlayer" controls>
-        <source id="audioSource" src="" type="audio/mp3">
-        Seu navegador não suporta o elemento de áudio.
-    </audio>
+    <div class="conteudo">
+        <?php 
+        $i = 0;
+        foreach ($audioFiles as $audioFile): 
+            $imgFile = $imgFiles[$i] ?? "";//existe? se n retorna vazio
+        ?>
+            <div class="card">
+                <div class="img">
+                    <img src="img/<?php echo $imgFile; ?>" alt="Imagem do conto" class="img">
+                </div>
+
+                <div onclick="playAudio('<?php echo $audioFile; ?>')"><?php echo $audioFile; ?></div>
+                <div class="audio">
+                    <audio id="audioPlayer" controls class="audio">
+                        <source id="audioSource" src="" type="audio/mp3">
+                        Seu navegador não suporta o elemento de áudio.
+                    </audio>
+                </div>
+            </div>
+        <?php 
+        $i++;
+        endforeach; 
+        ?>
+    </div>
 
     <script>
         function playAudio(fileName) {
