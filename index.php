1e5b858d92887c92ceefb431d18b326a9b292914
Aluno.Etec <aula@etec.com>
2025-10-24 21:53:43 -0300

    ;

--- index.php
+++ index.php
@@ -23,15 +23,20 @@ $tituloFiles = array_values(array_diff(scandir($tituloDir), array('.', '..')));
         <input onkeypress="buscar(event)" type="number" id="pesquisa" placeholder="Digite o número de telefone. . .">
     </header>
     <div class="tela-agradecimento" id="tela-agradecimento">
-        <p class="tituloTelaAgradecimento">Agradecimentos especiais</p>
-        <p style="margin-left: 10px">Ana Lúcia Cavalcante Sirino - 2º infonet</p>
-        <p style="margin-left: 10px">Luka de Moura Sanches - 2º infonet</p>
-        <p style="margin-left: 10px">Bárbara Fontanezzi Algarra - 2º infonet</p>
-        <p style="margin-left: 10px">Maria Eduarda Silva de Souza - 2º infonet</p>
-        <p style="margin-left: 10px">Isaac Gabriel Vieira Alves - 2º infonet</p>
-        <p style="margin-left: 10px">Miguel Angelo Monteiro Vicente - 2º infonet</p>
-        <p style="margin-left: 10px">Ana Vitória de Oliveira - 2º infonet</p>
-        <p style="margin-left: 10px">Emanuelly Guimãres Venâncio - 2º infonet</p>
+        <div class="imagemAgradecimento">
+            <img src="img/florAgradecimento.png" alt="Flor">
+        </div>
+        <div class="textoAgradecimento">
+            <p class="tituloTelaAgradecimento">Agradecimentos especiais</p>
+            <p style="margin-left: 10px">Ana Lúcia Cavalcante Sirino - 2º infonet</p>
+            <p style="margin-left: 10px">Luka de Moura Sanches - 2º infonet</p>
+            <p style="margin-left: 10px">Bárbara Fontanezzi Algarra - 2º infonet</p>
+            <p style="margin-left: 10px">Maria Eduarda Silva de Souza - 2º infonet</p>
+            <p style="margin-left: 10px">Isaac Gabriel Vieira Alves - 2º infonet</p>
+            <p style="margin-left: 10px">Miguel Angelo Monteiro Vicente - 2º infonet</p>
+            <p style="margin-left: 10px">Ana Vitória de Oliveira - 2º infonet</p>
+            <p style="margin-left: 10px">Emanuelly Guimãres Venâncio - 2º infonet</p>
+        </div>
     </div>
     <div class="conteudo" id="conteudo">
         <?php
